1bbda67569145db28647595d621cd9a61899a9d3
nKadykov <nkadykov@gmail.com>
2024-05-21 16:36:35 +0300

    15

--- mpi/15.cpp
+++ mpi/15.cpp
@@ -1,5 +1,12 @@
 #include <mpi.h>
 
-int main() {
+int main(int argc, char **argv) {
+	MPI_Init(&argc, &argv);
+	int rank;
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	int size;
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	printf("I am %d process from %d perocesses!\n", rank, size);
+	MPI_Finalize();
 	return 0;
 }
