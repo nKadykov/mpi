7ca4f79f036da707e29372e5913e70b917fe9adf
nKadykov <nkadykov@gmail.com>
2024-05-24 18:52:36 +0300

    fix: 27

--- mpi/27.cpp
+++ mpi/27.cpp
@@ -12,14 +12,20 @@ int main(int argc, char** argv) {
 	MPI_Comm_get_parent(&comm_parent);
 
 	if (comm_parent == MPI_COMM_NULL) {
-		int n = 0;
+		int n = 3;
 
-		char command[] = "E:/C/Visual Studio/mpi/x64/Debug/mpi";
-		MPI_Comm_spawn(command, NULL, n, MPI_INFO_NULL, 0, MPI_COMM_WORLD, &comm_inter, MPI_ERRCODES_IGNORE);
+		MPI_Comm_spawn((char *)"mpi", NULL, n, MPI_INFO_NULL, 0, MPI_COMM_WORLD, &comm_inter, MPI_ERRCODES_IGNORE);
 
 	}
 
-	std::cout << "I am " << rank << " process from " << size << "processes!\n";
+	std::cout << "I am " << rank << " process from " << size << " processes!\n";
+
+	if (comm_parent == MPI_COMM_NULL) {
+		std::cout << "My parent is " << rank << "\n";
+	}
+	else {
+		std::cout << "My parent is none\n";
+	}
 
 	MPI_Finalize();
 
